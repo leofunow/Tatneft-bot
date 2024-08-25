c0883abadfeba22b5e4dfec6545062d3cd0bb766
Иван Трушин <ivantrushin98@gmail.com>
2024-08-25 10:15:51 +0300

    retriever fix

--- src/sberrag.py
+++ src/sberrag.py
@@ -4,11 +4,15 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.retrievers import BM25Retriever
 from langchain_community.vectorstores import FAISS
+from langchain.retrievers import EnsembleRetriever
 import re
 from langchain.docstore.document import Document
+import pickle
 import logging
 from os import getenv
+import os
 import urllib
 import json
 
@@ -42,19 +46,61 @@ embeddings = HuggingFaceEmbeddings(
 def save_faiss():
     store.save_local("/aiogram_cache/faiss_index")
 
-# def load_faiss():
+def save_bm(retriever_to_safe, docs_list):
+    with open('/aiogram_cache/bm_retriever/bm25_retriever.pkl', 'wb') as f:
+        pickle.dump(retriever_to_safe, f)
+
+    with open('/aiogram_cache/bm_retriever/docs_list.pkl', 'wb') as f:
+        pickle.dump(docs_list, f)
+
+
 try:
     store = FAISS.load_local("/aiogram_cache/faiss_index", embeddings=embeddings, allow_dangerous_deserialization=True)
 except Exception as e:
     store = FAISS.from_documents(
     [Document(page_content="ошибка", metadata={"link": "error"})], embeddings)
     logging.info(e)
+
+if not os.path.exists("/aiogram_cache/bm_retriever"):
+    os.makedirs("/aiogram_cache/bm_retriever")
+
+bm_retriever = BM25Retriever.from_documents([Document(page_content="ошибка", metadata={"link": "error"})])
+with open('/aiogram_cache/bm_retriever/bm25_retriever.pkl', 'wb') as f:
+    pickle.dump(bm_retriever, f)
+docs_list = []
+with open('/aiogram_cache/bm_retriever/docs_list.pkl', 'wb') as f:
+    pickle.dump(docs_list, f)
+
+
+try:
+    with open('/aiogram_cache/bm_retriever/bm25_retriever.pkl', 'rb') as f:
+        bm_retriever = pickle.load(f)
+
+    with open('/aiogram_cache/bm_retriever/docs_list.pkl', 'rb') as f:
+        docs_list = pickle.load(f)
+except Exception as e:
+    bm_retriever = BM25Retriever.from_documents([Document(page_content="ошибка", metadata={"link": "error"})])
+    with open('/aiogram_cache/bm_retriever/bm25_retriever.pkl', 'wb') as f:
+        pickle.dump(bm_retriever, f)
+    docs_list = []
+    with open('/aiogram_cache/bm_retriever/docs_list.pkl', 'wb') as f:
+        pickle.dump(docs_list, f)
+
+ensemble_retriever = EnsembleRetriever(
+        retrievers=[bm_retriever, store.as_retriever()],
+         weights=[0.4, 0.6],
+    )
+
+
 # load_faiss()
 # store = store2
 text_splitter = RecursiveCharacterTextSplitter(separators="/n /n")
 
 
 def read_pdf(file_name):
+    global bm_retriever
+    global docs_list
+    global ensemble_retriever
     reader = PyPDFLoader(DOCS_FOLDER + file_name)
     pages = reader.load_and_split(text_splitter=text_splitter)
     for i in range(len(pages)):
@@ -66,17 +112,20 @@ def read_pdf(file_name):
         tmp = pattern.sub('', tmp)
         pages[i].page_content = tmp
         
+
     chunks_docs = [Document(page_content=pages[i].page_content, metadata={
                             "link": file_name}) for i in range(len(pages))]
     
     
-    
     store.add_documents(chunks_docs, embedding=embeddings)
+    docs_list = docs_list + chunks_docs
+    bm_retriever = BM25Retriever.from_documents(docs_list)
+    ensemble_retriever = EnsembleRetriever(
+        retrievers=[bm_retriever, store.as_retriever()], weights=[0.4, 0.6],
+    )
     save_faiss()
+    save_bm(bm_retriever, docs_list)
 
-
-# for doc in docs:
-#     read_pdf(doc)
 
 retriever = store.as_retriever()
 
@@ -87,7 +136,7 @@ def answer_sbert(question: str):
     for key in keywords.keys():
         question = question.replace(key, f"{key}({keywords[key]})")
         # question = question.replace(key.lower(), f"{key}({keywords[key]})")
-    context = retriever.invoke(question)
+    context = ensemble_retriever.invoke(question)[:4]
     messages = [
         SystemMessage(
             content=f"Ты - помощник по поиску информации, который использует извлеченные данные для генерации ответов на вопросы. Тебе предоставлен текстовый отрывок, в котором может содержаться информация, необходимая для ответа на вопрос. Если ты найдешь нужную информацию в тексте, используй ее для составления ответа. Если необходимой информации в тексте нет, просто ответь (Не знаю)./nКонтекст: {context}"
